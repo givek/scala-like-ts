9b1cf843a111fe0a547402e7c6c8593f718be5c9
Vivek Gandharkar <vivekgandharkar@outlook.com>
2024-02-03 23:32:17 +0530

    add groupBy

---
+++ src/utils/groupBy.ts
@@ -0,0 +1,20 @@
+type RecordKey = string | number | symbol;
+
+function groupBy<T extends Record<RecordKey, unknown>, R extends RecordKey>(
+  l: T[],
+  f: (i: T) => R
+): Record<R, T[]> {
+  let result: Record<R, T[]> = {} as Record<R, T[]>;
+
+  l.forEach((v) => {
+    const key = f(v);
+
+    if (result[key]) {
+      result[key].push(v);
+    } else {
+      result = { ...result, [key]: [v] };
+    }
+  });
+
+  return result;
+}
